0ba1d5cbf8fd17a08c125c0569469fc826c0c687
Caroline Castro <carolinecg431@gmail.com>
2024-09-16 23:46:10 -0300

    criação do Modulo da Pokedex do aplicativop

--- ProjetoCapacitacao/app/src/main/java/com/example/pokedex/api/RetrofitInstance.kt
+++ ProjetoCapacitacao/app/src/main/java/com/example/pokedex/api/RetrofitInstance.kt
@@ -6,14 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 //classe que vai acessar a api
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://pokeapi.co/api/v2/"
+     const val BASE_URL = "https://pokeapi.co/api/v2/"
 
-    // Instância única de Retrofit para ser utilizada no app
-    val api: PokeApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(PokeApiService::class.java)
-    }
+    // Instância única de Retrofit para ser utilizada no app será peag
+
 }
--- ProjetoCapacitacao/app/src/main/java/com/example/pokedex/di/AppModule.kt
+++ ProjetoCapacitacao/app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -1,10 +1,35 @@
 package com.example.pokedex.di
 
+import com.example.pokedex.api.PokeApiService
+import com.example.pokedex.api.RetrofitInstance.BASE_URL
+import com.example.pokedex.repository.PokemonRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+//Aqui  fornece o repositório pokemon para criar a  Pokedex do app
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApiService
+    ) = PokemonRepository(api)
+
+   // Instância única de Retrofit para ser utilizada no app, pega o link do RetrofitInstance
+    @Singleton
+   @Provides
+    fun providePokeApiService(): PokeApiService{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create()) // cria e converte o Json em classes de dados
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApiService::class.java)
+    }
+
 }
